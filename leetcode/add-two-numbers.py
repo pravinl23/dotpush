28479b29c37f051443625eb85aa5b904e8dec3b9
Pravin Lohani <116219173+pravinl23@users.noreply.github.com>
2025-04-26 20:55:50 -0400

    updated add-two-numbers solution

--- leetcode/add-two-numbers.py
+++
@@ -1,9 +0,0 @@
-        :type l1: Optional[ListNode]
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
